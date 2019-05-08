841acba249d52b05786488e57d3836dad6eb475d
AirRain <31362594+airrain@users.noreply.github.com>
2019-05-08 10:01:59 +0800

    update
    update

---
+++ sanguo/utils/sgs.js
@@ -0,0 +1,12 @@
+function getAnswer(){
+  var data = [{"id":1}];
+  return data;
+}
+function getResult(){
+  var result = {"A":name};
+  return result;
+}
+module.exports = {
+  getAnswer:getAnswer,
+  getResult:getResult
+}
