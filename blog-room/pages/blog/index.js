13fb7b0d7221f11b8521d83f4d2d835347da418c
AirRain <31362594+airrain@users.noreply.github.com>
2019-09-12 18:48:59 +0800

    update

--- blog-room/pages/blog/index.js
+++ blog-room/pages/blog/index.js
@@ -3,6 +3,28 @@ var feed = require('../../utils/feed.js')
 var common = require('../../utils/common.js')
 var app = getApp()
 Page({
-    
+    data:{
+        feeds:{},
+    },
+    updateNews:function(){
+        var that = this
+        wx.request({
+            url:common.baseUrl + 'news_rss.python',
+            data:{},
+            header:{
+                'Content-Type':'application/json'
+            },
+            success:function(res){
+                var feeds = feed.getItems(res.data);
+                wx.setStorage({
+                    key:"news_feeds",
+                    data:feeds
+                })
+                that.setData({
+                    feeds:feeds
+                })
+            }
+        })
+    }
 
 })
